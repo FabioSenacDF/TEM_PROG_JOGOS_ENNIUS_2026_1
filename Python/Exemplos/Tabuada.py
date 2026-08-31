a74bac675aeb080ad9fd176af68107b4de8d3761
FabioSenacDF <fabio.6141@df.senac.br>
2026-08-31 15:34:09 -0300

    Create Tabuada.py

---
+++ Python/Exemplos/Tabuada.py
@@ -0,0 +1,8 @@
+# Receber um numero com input e converter para int
+# fazer um loop mostrando a tabuado deste numero
+# de 1 a 10
+numero = int(input("Digite um numero para saber a tabuada: "))
+
+for i in range(1,11):
+    resultado = numero * i
+    print(resultado)
