2f9a9f9827d900a1d2174dac12f97fca3405720c
FabioSenacDF <fabio.6141@df.senac.br>
2026-09-08 16:47:28 -0300

    Update Dicionario.py

--- Python/Estruturas_de_Dados/Dicionario.py
+++ Python/Estruturas_de_Dados/Dicionario.py
@@ -42,31 +42,69 @@
 # print(perfil_game)
 
 ##############################################
-# Remover elementos
-carrinho_compras = {
-    "mouse" : 79.99,
-    "teclado_gamer" : 249.99,
-    "monitor" : 1499.49,
-    "placa_RTX" : 29999.00
-}
+# # Remover elementos
+# carrinho_compras = {
+#     "mouse" : 79.99,
+#     "teclado_gamer" : 249.99,
+#     "monitor" : 1499.49,
+#     "placa_RTX" : 29999.00
+# }
 
-# Anular o valor 
-carrinho_compras["teclado_gamer"] = None
+# # Anular o valor 
+# carrinho_compras["teclado_gamer"] = None
 
-# Remove e retorna valor
-preco_mouse = carrinho_compras.pop("mouse")
-print("Preço do mouse:",preco_mouse)
+# # Remove e retorna valor
+# preco_mouse = carrinho_compras.pop("mouse")
+# print("Preço do mouse:",preco_mouse)
 
-# Remove par sem retornar
-del carrinho_compras["monitor"]
+# # Remove par sem retornar
+# del carrinho_compras["monitor"]
 
-# Remove ultimo par (chave:valor) como tupla
-ultimo = carrinho_compras.popitem()
-print(ultimo)
+# # Remove ultimo par (chave:valor) como tupla
+# ultimo = carrinho_compras.popitem()
+# print(ultimo)
 
-# Limpar Dicionário
-carrinho_compras.clear()
+# # Limpar Dicionário
+# carrinho_compras.clear()
 
-print(carrinho_compras)
+# print(carrinho_compras)
+
 ##############################################
+# Iteração (laços de repetição)
 
+#faturamento = {"jan":1000, "fev":1500, "mar":1800}
+
+#for mes in faturamento.keys():  # Iteração nas chaves
+    #print(mes)
+
+#for val in faturamento.values(): # Iteração nos valores
+    #print(val)
+
+#for mes,val in faturamento.items():# Iteração no par
+    #print(f"No mês {mes} o faturamento foi R${val}")
+
+##############################################
+# Dicionário Aninhado, similar a JSON
+
+banco_de_dados = {
+    "cliente_1":{
+        "nome":"Ana",
+        "compras":["Livro","Caneta"]
+    },
+    "cliente_2":{
+        "nome":"Bruno",
+        "compras":["Caderno","Tesoura"]
+    },
+    "cliente_3":{
+        "nome":"Caio",
+        "compras":["Borracha","Livro"]
+    }
+}
+nome_cliente_1 = banco_de_dados["cliente_1"]["nome"]
+print(nome_cliente_1)
+
+primeira_compra_bruno = banco_de_dados["cliente_2"]["compras"][0]
+print(primeira_compra_bruno)
+
+lista_compras_caio = banco_de_dados["cliente_3"]["compras"]
+print(lista_compras_caio)
